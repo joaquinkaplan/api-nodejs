fbd178f27ae725b1635074d96139022b49d68070
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-09-28 23:13:56 -0300

    .

---
+++ config/mongo.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbConnect = () => {
+  const DB_URI = process.env.DB_URI;
+  mongoose.connect(
+    DB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err, res) => {
+      if (!err) {
+        console.log("*****CONNECTION SUCCESSFULL***");
+      } else {
+        console.log("****CONNECTION ERROR!!****");
+      }
+    }
+  );
+};
+
+module.exports = dbConnect;
