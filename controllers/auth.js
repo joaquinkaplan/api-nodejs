4bdfd7881eb00e88fcca3d9817bcfbf777ac04a6
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-10-06 22:08:18 -0300

    login jwt config

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const { matchedData } = require("express-validator");
-const { encrypt } = require("../utils/handlePassword");
+const { encrypt, compare } = require("../utils/handlePassword");
 const { tokenSign } = require("../utils/handleJwt");
 const { usersModel } = require("../models");
+const { handleHttpError } = require("../utils/handleError");
 
 const registerCtrl = async (req, res) => {
   try {
@@ -15,8 +16,39 @@ const registerCtrl = async (req, res) => {
 
     res.send({ data });
   } catch (e) {
-    res.status(400).send({ error: e });
+    handleHttpError(res, "ERROR_REGISTER_USER");
   }
 };
 
-module.exports = { registerCtrl };
+const loginCtrl = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const user = await usersModel
+      .findOne({ email: req.email })
+      .select("password name role email");
+    if (!user) {
+      handleHttpError(res, "USER_NOT_EXISTS", 404);
+      return;
+    }
+
+    const hashPassword = user.get("password");
+    const check = await compare(req.password, hashPassword);
+
+    if (!check) {
+      handleHttpError(res, "PASSWORD_INVALID", 401);
+      return;
+    }
+
+    user.set("password", undefined, { strict: false });
+    const data = {
+      token: await tokenSign(user),
+      user,
+    };
+
+    res.send({ data });
+  } catch (e) {
+    handleHttpError(res, "ERROR_LOGIN_USER");
+  }
+};
+
+module.exports = { registerCtrl, loginCtrl };
